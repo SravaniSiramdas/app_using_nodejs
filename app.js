8be71a57fd1269969a13d5adfa70ec1dd4fbd5aa
SravaniSiramdas <54675854+SravaniSiramdas@users.noreply.github.com>
2019-11-06 13:10:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ const request = require('request');
 var url = require('url');
 var moment = require('moment');
 var schedule = require('node-schedule');
-let link = "https://21sgrosbv7.execute-api.ap-southeast-1.amazonaws.com/cookie1?token_value=Chdhhd3Fsh5887ShjhA200Rr&amp;amp;start_date=" + previousHour() + "&amp;amp;end_date=" + presentHour() + "&amp;amp";
-
-
 let payLoadObject = new URLSearchParams({
   "v": "1",
   "tid": "UA-147234783-1",
@@ -16,7 +13,7 @@ let payLoadObject = new URLSearchParams({
 
 var rule = new schedule.RecurrenceRule();
 var j = schedule.scheduleJob(rule, function() {
-  rule.minute = 20;
+  rule.minute = 30;
   getRequest();
 });
 
@@ -29,16 +26,12 @@ function previousHour() {
 }
 
 function getRequest() {
+  let link = "https://21sgrosbv7.execute-api.ap-southeast-1.amazonaws.com/cookie1?token_value=Chdhhd3Fsh5887ShjhA200Rr&amp;amp;start_date=" + previousHour() + "&amp;amp;end_date=" + presentHour() + "&amp;amp";
   request(link, function(err, response, body) {
     if (err) {
       console.log(err);
     } else {
       let data = JSON.parse(body);
-      if(data === NULL){
-        payLoad = "NO DATA";
-        sendPostRequest(payLoad);
-      }
-      else
       FormationOfPayload(data);
     }
 
@@ -47,25 +40,25 @@ function getRequest() {
 
 function FormationOfPayload(data) {
   data.forEach(function(object) {
-    payLoadObject.append('cid', object.cookie_id);
-    payLoadObject.append('cd1', object.b1_AccountId);
-    payLoadObject.append('cd2', object.last_event_ts);
-    payLoadObject.append('cd3', object.visit_count);
-    payLoadObject.append('cd4', object.page_count);
-    payLoadObject.append('cd5', object.exit_page_url);
-    payLoadObject.append('cd6', object.active_time);
-    payLoadObject.append('cd7', object.passive_time);
-    payLoadObject.append('cd8', object.click_count);
-    payLoadObject.append('cd9', object.click_engagement);
-    payLoadObject.append('cd10', object.scroll_time);
-    payLoadObject.append('cd11', object.scroll_attention_time);
-    payLoadObject.append('cd12', object.hover_attention_time);
-    payLoadObject.append('cd13', object.hover_time);
-    payLoadObject.append('cd14', object.hover_engagement_time);
-    payLoadObject.append('cd15', object.eng_score);
-    payLoadObject.append('cd16', object.eng_score_norm);
-    payLoadObject.append('cd17', object.key_exposure_content);
-    payLoadObject.append('cd18', object.key_engagement_content);
+    payLoadObject.set('cid', object.cookie_id);
+    payLoadObject.set('cd1', object.b1_AccountId);
+    payLoadObject.set('cd2', object.last_event_ts);
+    payLoadObject.set('cd3', object.visit_count);
+    payLoadObject.set('cd4', object.page_count);
+    payLoadObject.set('cd5', object.exit_page_url);
+    payLoadObject.set('cd6', object.active_time);
+    payLoadObject.set('cd7', object.passive_time);
+    payLoadObject.set('cd8', object.click_count);
+    payLoadObject.set('cd9', object.click_engagement);
+    payLoadObject.set('cd10', object.scroll_time);
+    payLoadObject.set('cd11', object.scroll_attention_time);
+    payLoadObject.set('cd12', object.hover_attention_time);
+    payLoadObject.set('cd13', object.hover_time);
+    payLoadObject.set('cd14', object.hover_engagement_time);
+    payLoadObject.set('cd15', object.eng_score);
+    payLoadObject.set('cd16', object.eng_score_norm);
+    payLoadObject.set('cd17', object.key_exposure_content);
+    payLoadObject.set('cd18', object.key_engagement_content);
     console.log(payLoadObject.toString());
     sendPostRequest(payLoadObject.toString());
   });
